dd77e73f7c5098c3f6d7352c50e76766f4b5aea9
Le Gia Luan <legialuan780@gmail.com>
2025-05-02 20:37:54 +0700

    cap nhat chuc nang QR

---
+++ src/main/java/dto/PaymentRequest.java
@@ -0,0 +1,52 @@
+package com.cinema.dto;
+
+import com.cinema.enums.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public class PaymentRequest {
+    private String accountId;
+    private BigDecimal amount;
+    private String description;
+    private PaymentMethod paymentMethod;
+
+    public PaymentRequest(String accountId, BigDecimal amount, String description, PaymentMethod paymentMethod) {
+        this.accountId = accountId;
+        this.amount = amount;
+        this.description = description;
+        this.paymentMethod = paymentMethod;
+    }
+
+    // Getters và Setters
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+}
